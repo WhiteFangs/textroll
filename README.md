9045dd5d7085714c8c20eb791594b9014e16c05d
WhiteFangs <white-fangs@hotmail.fr>
2017-02-01 00:47:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# textroll
-A small javascript module to animate text replacement by rolling through the characters until reaching the new one
+# Textroll
+
+Textroll is a small javascript module to animate text replacement with a rolling effect for each character.
+
+The characters "roll" through the set of characters they belong to until they reach the character by which they are replaced in the new text.
+
+## Example
+
+An image is worth a thousands words, so a gif probably does worth more:
+
+![](./example.gif)
+
+The transition works especially well on replacement of text with the same beginning:
+
+![](./interesting.gif)
+
+## Install
+
+Simply add the textroll.js script file to your page
+
+```html
+<script src="./textroll.js"></script>
+
+```
+
+## Options
+
+You can initialize textroll with options, here are the default values for the available options:
+
+```javascript
+
+var defaultOptions = {
+	'interval' : 100 // milliseconds between each iterations of replacement
+	'alphabet' : 'abcdefghijklmnopqrstuvwxyz' // string of successive letters to add
+	'numbers' : '0123456789' // string of successive letters to add
+	'punctuation' : '"\'(-),?;.:!' // string of successive letters to add
+	'specials' : '&#~{[|`_\\^@]=}+°<>/§*%¨$£¤' // string of successive letters to add
+	'addCorpora' : {'corpusName' : ''} // add new corpus, object with key: corpus name, value: strings of successive characters,
+	'spaceCorpus' : 'alphabet' // string defining which corpus contains space character
+	'changeCase' : 'end' // 'beginning' or 'end' for applying case change
+}
+
+// use init to override default options with your own
+textroll.init({
+	'interval' : 500, 
+	'spaceCorpus' : 'punctuation',
+	'addCorpora': {'cedilla' : 'çş'}
+	'changeCase' : 'beginning'
+});
+
+```
---
+++ textroll.js
@@ -0,0 +1,144 @@
+var textroll = (function () {
+
+	var _defaults = {
+		interval: 100,
+		alphabet : 'abcdefghijklmnopqrstuvwxyz'.split(''),
+		numbers : '0123456789'.split(''),
+		punctuation: '"\'(-),?;.:!'.split(''),
+		specials: '&#~{[|`_\\^@]=}+°<>/§*%¨$£¤'.split(''),
+		spaceCorpus : "alphabet",
+		changeCase: "end"
+	}
+
+	var _options;
+
+	function init(options){
+		_options = {};
+		options = options || {};
+		// build options with corpora first
+		_options.alphabet = options.alphabet != undefined ? options.alphabet : _defaults.alphabet;
+		_options.numbers = options.numbers != undefined ? options.numbers : _defaults.numbers;
+		_options.punctuation = options.punctuation != undefined ? options.punctuation : _defaults.punctuation;
+		_options.specials = options.specials != undefined ? options.specials : _defaults.specials;
+		// add other corpora
+		var newCorpora = options.addCorpora;
+		for (var k in newCorpora){
+			if (newCorpora.hasOwnProperty(k) && _options[k] == undefined) {
+				_options[k] = newCorpora[k].split('');
+			}
+		}
+		// add space to corresponding corpora
+		if(options.spaceCorpus != undefined && _options[options.spaceCorpus] != undefined){
+			_options[options.spaceCorpus] = (' ' + _options[options.spaceCorpus].join('')).split('')
+		}else{
+			_options[_defaults.spaceCorpus] = (' ' + _options[_defaults.spaceCorpus].join('')).split('')
+		}
+		// build corpora
+		_options.corpora = [];
+		for (var k in _options){
+			if (_options.hasOwnProperty(k) && k != "corpora") {
+				_options.corpora.push(_options[k]);
+			}
+		}
+		// add other variables to _options
+		_options.interval = options.interval != undefined ? options.interval : _defaults.interval;
+		_options.changeCaseAtEnd = options.changeCase == "beginning" ? false : options.changeCase == "end" ? true : _defaults.changeCase == "end";
+	}		
+	
+	function sansAccents(s) {
+		var r = s.toLowerCase();
+		r = r.replace(/[àáâãäå]/g, "a");
+		r = r.replace(/[èéêë]/g, "e");
+		r = r.replace(/[ìíîï]/g, "i");
+		r = r.replace(/[òóôõö]/g, "o");
+		r = r.replace(/[ùúûüµ]/g, "u");
+		r = r.replace(/[ýÿ]/g, "y");
+		return r;
+	};
+
+	function isCapital(letter){
+		return _options.alphabet.indexOf(letter.toLowerCase()) > 0 && letter.toUpperCase() == letter; // space is not cap
+	}
+
+	function replaceCharAt(word, i, newChar){
+		return word.slice(0, i) + newChar + word.slice(i+1, word.length);
+	}
+
+	function getCorpus(character){
+		var i = 0, idx = -1, corpus;
+		while(idx < 0 && i < _options.corpora.length){
+			corpus = _options.corpora[i];
+			idx = corpus.indexOf(character);
+			i++;
+		}
+		return corpus;
+	}
+
+	function replaceText (element, newText){
+		var oldText = element.innerText;
+		var interval = setInterval(function (){
+			if( oldText == newText || oldText.toLowerCase() == sansAccents(newText)){
+				element.innerText = newText;
+				clearInterval(interval);
+				return;
+			}else{
+				oldText = getNextText(oldText, newText);
+				if(oldText.length > newText.length)
+					oldText = oldText.slice(0, oldText.length - 1);
+				element.innerText = oldText;
+			}
+		}, _options.interval);
+	}
+
+	function getNextText(oldText, newText){
+		for(var i =0; i < newText.length; i++){
+			var oldChar = oldText[i];
+			var newChar = newText[i];
+			if(oldChar == newChar)
+				continue;
+
+			var isNewCap = isCapital(newChar);
+			newChar = sansAccents(newChar);
+			var newCorpus = getCorpus(newChar);
+			if(newCorpus.indexOf(newChar) < 0) // if character is unknown
+				newCorpus = [newChar];
+			
+			if(oldChar == undefined){ // newText is longer, so we finish this round by adding newChar at the end of oldText
+				oldText += newCorpus[0];
+				i = newText.length;
+				continue;
+			}
+
+			var isOldCap = isCapital(oldChar);
+			oldChar = sansAccents(oldChar);
+			if(newChar == oldChar){
+				oldText = isNewCap ? replaceCharAt(oldText, i, newChar.toUpperCase()) : replaceCharAt(oldText, i, newChar);
+			}else{
+				var oldCorpus = getCorpus(oldChar);
+				var nextChar;
+				var oldIdx = oldCorpus.indexOf(oldChar);
+				if(oldIdx < 0){
+					nextChar = newCorpus[0];
+				}else if(newCorpus == oldCorpus){
+					nextChar = oldCorpus[(oldIdx + 1) % oldCorpus.length];
+				}else if(oldIdx == oldCorpus.length - 1){
+					nextChar = newCorpus[0];
+				}else{
+					nextChar = oldCorpus[oldIdx + 1];
+				}
+				if((_options.changeCaseAtEnd && isOldCap) || (!_options.changeCaseAtEnd && isNewCap))
+					nextChar = nextChar.toUpperCase();
+
+				oldText = replaceCharAt(oldText, i, nextChar);
+			}
+		}
+		return oldText;
+	}
+
+	return {
+		init: init,
+		replace : replaceText
+	};
+})();
+
+textroll.init();
